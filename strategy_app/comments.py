e39cf57d7f4bb8f7013e47ceb9ca4ec81786d0e5
Enzo La Spada <elaspada96@gmail.com>
2020-11-23 20:44:06 +0100

    Added the comment post route and comment view

--- strategy_app/comments.py
+++ strategy_app/comments.py
@@ -8,3 +8,13 @@ second = Blueprint("second", __name__)
 def comment_new(_id=None):
 	strat = Strategy.objects(id=_id).first()
 	return render_template("comments/new.html", Strat=strat)
+
+@second.route("", methods=["POST"])
+def comment_post(_id=None):
+	newComment = Comment(content=request.form["content"])
+	strat = Strategy.objects(id=_id).first()
+	strat.comments.append(newComment)
+	strat.save()
+	for strat in Strats:
+		strat.reload()
+	return redirect("/strategies/" + _id)
